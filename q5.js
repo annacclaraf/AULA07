f2461f9a6d8a41e1235b84af02c2350ef5ea0f09
annacclaraf <anna.bento@estudante.iftm.edu.br>
2025-08-13 17:46:34 -0300

    Add files via upload

--- q5.js
+++ q5.js
@@ -3,4 +3,5 @@ let idade =Number(prompt("Digite sua idade em anos:"))
 let meses = (idade * 12)
 let dias = (idade * 365)
 
-console.log("")
+console.log("Você tem aproximadamente " + meses + " meses de vida.");
+console.log("Você tem aproximadamente " + dias + " dias de vida.");
